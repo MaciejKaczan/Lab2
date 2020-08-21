ed01fc5a0d6373fe653555d485c2f7131a91343e
MaciejKaczan <MaciejKaczan@users.noreply.github.com>
2020-08-21 19:49:40 +0200

    test1

--- Lab2/OutsideWithDoor.cs
+++ Lab2/OutsideWithDoor.cs
@@ -6,6 +6,7 @@ namespace Lab2
 {
     class OutsideWithDoor : Room, IHasExteriorDoor
     {
+        private string Test1;
         public string DoorDescription { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string DoorLocation { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
